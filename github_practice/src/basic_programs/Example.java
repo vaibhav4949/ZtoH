5eb5fbc65c4abedb9bc67c967ceda10f005cc93f
vaibhav4949 <kangudevaibhav07@gmail.com>
2023-07-14 14:58:24 +0530

    Multiplication or Equal

--- github_practice/src/basic_programs/Example.java
+++ github_practice/src/basic_programs/Example.java
@@ -2,11 +2,11 @@ package basic_programs;
 
 public class Example 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("hello world");
 		System.out.println("hi india");
 
 		System.out.println("hi world");
-
 	}
 }
