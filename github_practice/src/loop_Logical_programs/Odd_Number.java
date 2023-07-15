29c535508e7f2546c4449b9d64c59db8025e069a
vaibhav4949 <kangudevaibhav07@gmail.com>
2023-07-15 15:48:45 +0530

    Odd Number

---
+++ github_practice/src/loop_Logical_programs/Odd_Number.java
@@ -0,0 +1,23 @@
+package loop_Logical_programs;
+
+import java.util.Scanner;
+
+public class Odd_Number 
+{
+	public static void main(String[]args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Your Number");
+		int n = sc.nextInt();
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+		sc.close();
+	}
+}
