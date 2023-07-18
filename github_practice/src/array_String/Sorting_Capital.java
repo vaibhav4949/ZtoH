5e92efbdf9a40f0c657dee5b0d281ca0454b1b55
vaibhav4949 <kangudevaibhav07@gmail.com>
2023-07-18 11:11:41 +0530

    Sorting_Capital

---
+++ github_practice/src/array_String/Sorting_Capital.java
@@ -0,0 +1,30 @@
+package array_String;
+
+import java.util.Scanner;
+
+public class Sorting_Capital 
+{
+	public static void main(String[]args) 
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the String");
+		String str = sc.nextLine();
+		char arr[] = str.toCharArray();
+		sc.close();
+
+		int i=0;
+		char temp;
+
+		for(i=0;i<arr.length/2;i++)
+		{
+			temp = arr[i];
+			arr[i]= arr[arr.length-1-i];
+			arr[arr.length-1-i] = temp;
+		}
+		for(int k=0;k<arr.length;k++)
+		{
+			System.out.print(arr[k]);
+		}
+	}
+}
