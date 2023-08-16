b42b8015a1e8c84438897577388861ac9cfc6e45
vaibhav4949 <kangudevaibhav07@gmail.com>
2023-08-16 16:53:20 +0530

    exception_handling

---
+++ Java/src/exception_handeling/Basic_Of_Exception_Handeling.java
@@ -0,0 +1,20 @@
+package exception_handeling;
+
+public class Basic_Of_Exception_Handeling 
+{
+	public static void main(String[]args) 
+	{
+		// An exception is an unwanted event,which occurs during the execution of  
+		// program i.e.at runtime,that disturb the normal flow of the program.
+		// All the Exception occures during runtime.
+
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println(50/0);   // This will disturb the normal flow of program.
+		System.out.println("6");    // Show Arithmatic exception.
+		System.out.println("7");
+		System.out.println("8");
+	}
+}
