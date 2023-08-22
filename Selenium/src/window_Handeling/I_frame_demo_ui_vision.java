3e2d97c81645868eb5cd76826a0f19f4da32e728
vaibhav4949 <kangudevaibhav07@gmail.com>
2023-08-22 18:46:36 +0530

    Update I_frame_demo_ui_vision.java

--- Selenium/src/window_Handeling/I_frame_demo_ui_vision.java
+++ Selenium/src/window_Handeling/I_frame_demo_ui_vision.java
@@ -1,7 +1,6 @@
 package window_Handeling;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
